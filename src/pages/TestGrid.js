6f2c2bbbc1a92d8741cdd2f0735fa75ec912f1a7
Swammered <jamesswim12@yahoo.com>
2022-09-19 14:20:58 -0400

    Added button/Link and grid

--- src/pages/TestGrid.js
+++ src/pages/TestGrid.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Typography, Grid } from '@mui/material'
+import { Typography, Grid, Button} from '@mui/material'
+import { Link } from 'react-router-dom'
 
 
 
@@ -7,15 +8,20 @@ const TestGrid = () => {
 
     return (
         <>        
-            <Grid container>
-                <Grid item>hi</Grid>
-                <Grid item>hello</Grid>
+            <Grid container spacing={2}>
+                <Grid item xs={6} md={2}>hi</Grid>
+                <Grid item xs={6} md={2}>hello</Grid>
+                <Grid item xs={6} md={2}>yo</Grid>
+                <Grid item xs={6} md={2}>helllllllo</Grid>
+                <Grid item xs={6} md={2}>hiya</Grid>
+                <Grid item xs={6} md={2}>heyo</Grid>
 
             </Grid>
+
             <Typography>I'm the TestGrid</Typography>
+
+            <Button component={Link} to='/search' variant='outlined'>Search</Button>
         </>
-
-
     )
 }
 
