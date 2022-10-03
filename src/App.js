e15fc55befc2ab3d21ce3a05142d5149ce2bee5b
Swammered <jamesswim12@yahoo.com>
2022-10-03 14:11:47 -0400

    removed more unused variable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Typography } from '@mui/material';
-import SaveIcon from '@mui/icons-material/Save'
 import Layout from './components/Layout';
-import TestGrid from './pages/TestGrid';
 
 
 function App() {
